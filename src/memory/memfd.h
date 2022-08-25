4ea6577caa8778c92577e89a600268b14fe61521
tscmoo <the.dengus@gmail.com>
2022-08-25 16:03:28 -0700

    Memfd allocator.
    This introduces a memory allocator based on the memfd_create linux system call.
    memfd_create creates an in-memory file, which can be mmapped and shared between processes. This file is automatically deleted (and memory freed) when the last process using it exits.
    This will be the basis of future faster shared memory transports, and gives us firmer control over memory (eg for memory locking, communicating with cuda/infiniband)

---
+++ src/memory/memfd.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <array>
+#include <atomic>
+#include <cstdlib>
+#include <memory>
+#include <utility>
+
+namespace rpc {
+namespace memfd {
+
+struct Memfd {
+  int fd = -1;
+  void* base = nullptr;
+  size_t size = 0;
+
+  Memfd() = default;
+  ~Memfd();
+  Memfd(const Memfd&) = delete;
+  Memfd(Memfd&& n) {
+    std::swap(fd, n.fd);
+    std::swap(base, n.base);
+    std::swap(size, n.size);
+  }
+  Memfd& operator=(const Memfd&) = delete;
+  Memfd& operator=(Memfd&& n) {
+    std::swap(fd, n.fd);
+    std::swap(base, n.base);
+    std::swap(size, n.size);
+    return *this;
+  }
+
+  static Memfd create(size_t size);
+  static Memfd map(int fd, size_t size);
+};
+
+struct AddressInfo {
+  int fd = -1;
+  size_t fdSize = 0;
+  size_t offset = 0;
+};
+
+struct MemfdAllocatorImpl;
+struct MemfdAllocator {
+  std::unique_ptr<MemfdAllocatorImpl> impl;
+  MemfdAllocator();
+  ~MemfdAllocator();
+  std::pair<void*, size_t> getMemfd(int fd);
+  AddressInfo getAddressInfo(void* ptr);
+  std::pair<void*, size_t> allocate(size_t size);
+  void deallocate(void* ptr, size_t size);
+};
+
+} // namespace memfd
+
+} // namespace rpc
